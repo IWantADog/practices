25c8892a27ce2c4c7822bb15afba15c005c1bcab
shuai.li <shuai.li@gamesvessel.com>
2021-10-21 20:32:56 +0800

    [ADD] src log

--- src/server.py
+++ src/server.py
@@ -2,15 +2,17 @@ from concurrent import futures
 import logging
 import grpc
 
+logging.basicConfig()
+logger = logging.getLogger(__name__)
 main_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
 
 def serve():
+    logger.info("server start")
     main_server.add_insecure_port('[::]:50051')
     main_server.start()
     main_server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
