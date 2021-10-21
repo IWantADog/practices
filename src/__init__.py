ed022d83b4488a446157597f7c07f36331009f82
shuai.li <shuai.li@gamesvessel.com>
2021-10-21 20:30:19 +0800

    [ADD] add src

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,19 @@
+import logging
+
 from grpc_python.greeter_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
 
 from grpc_python.greeter_pb2 import HelloReply
+from src.server import main_server
 
-import logging
 
 class Greeter(GreeterServicer):
-	def SayHello(self, request, context):
-		logging.info(f'receive a request {request.name}')
-		return HelloReply(message=f"Hello, {request.name}")
+    def SayHello(self, request, context):
+        logging.info(f'receive a request {request.name}')
+        return HelloReply(message=f"Hello, {request.name}")
 
-	def SayHelloAgain(self, request, context):
-		logging.info(f'receive a request {request.name}')
-		return HelloReply(message=f"Hello again, {request.name}")
+    def SayHelloAgain(self, request, context):
+        logging.info(f'receive a request {request.name}')
+        return HelloReply(message=f"Hello again, {request.name}")
 
 
-customer_services = [Greeter]
+add_GreeterServicer_to_server(Greeter(), main_server)
--- src/server.py
+++ src/server.py
@@ -1,27 +1,16 @@
-from grpc.greeter_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
-from grpc.greeter_pb2 import HelloReply
-
 from concurrent import futures
 import logging
 import grpc
 
-class Greeter(GreeterServicer):
-	def SayHello(self, request, context):
-		logging.info(f'receive a request {request.name}')
-		return HelloReply(message=f"Hello, {request.name}")
-
-	def SayHelloAgain(self, request, context):
-		logging.info(f'receive a request {request.name}')
-		return HelloReply(message=f"Hello again, {request.name}")
+main_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
 
 def serve():
-	server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-	add_GreeterServicer_to_server(Greeter(), server)
-	server.add_insecure_port('[::]:50051')
-	server.start()
-	server.wait_for_termination()
+    main_server.add_insecure_port('[::]:50051')
+    main_server.start()
+    main_server.wait_for_termination()
+
 
 if __name__ == '__main__':
-	logging.basicConfig()
-	serve()
+    logging.basicConfig()
+    serve()
