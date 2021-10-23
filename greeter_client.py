4ddcd8252dc72a690e20b602830b8c68fab4b8a6
benjilee <lsdk5453@gmail.com>
2021-10-23 15:47:45 +0800

    [MODIFY] modify client

--- greeter_client.py
+++ greeter_client.py
@@ -1,15 +1,17 @@
 import grpc
-from grpc.greeter_pb2_grpc import GreeterStub
-from grpc.greeter_pb2 import HelloRequest
+from grpc_python.greeter_pb2_grpc import GreeterStub
+from grpc_python.greeter_pb2 import HelloRequest
+
 
 def run():
-	channel = grpc.insecure_channel('localhost:50051')
-	stub = GreeterStub(channel=channel)
-	response = stub.SayHello(HelloRequest(name='you'))
-	print(f"this is first response {response.message}")
+    channel = grpc.insecure_channel('localhost:5000')
+    stub = GreeterStub(channel=channel)
+    response = stub.SayHello(HelloRequest(name='you'))
+    print(f"this is first response {response.message}")
 
-	response = stub.SayHelloAgain(HelloRequest(name='you'))
-	print(f"this is the second response {response.message}")
+    response = stub.SayHelloAgain(HelloRequest(name='you'))
+    print(f"this is the second response {response.message}")
+
 
 if __name__ == '__main__':
-	run()
+    run()
